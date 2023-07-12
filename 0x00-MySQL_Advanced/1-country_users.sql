f5912f1914b26ffb7ec59a5e097b96c882f818ef
AishaKhalfan <aishakhalfan45@gmail.com>
2023-07-13 01:15:18 +0300

    Enumerated countries

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- a script that creates the table user
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+);
