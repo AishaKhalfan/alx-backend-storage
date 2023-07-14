eb07f3161c77233d58a5285e20e4b4606b0a13b2
AishaKhalfan <aishakhalfan45@gmail.com>
2023-07-14 10:45:17 +0300

    Bookinfo database dump

--- 0x00-MySQL_Advanced/aisha.sql
+++ 0x00-MySQL_Advanced/aisha.sql
@@ -19,3 +19,30 @@ UNIQUE INDEX ( ID )
 -- add an index to existing table
 CREATE INDEX indexName ON aisha2 (ID, LName, FName, LOC);
 
+-- View without column_list
+SELECT * FROM users;
+CREATE VIEW my_v1 AS SELECT * FROM users;
+
+-- View with column_list
+CREATE VIEW my_v2 AS 
+SELECT id AS USER_ID,
+email AS USER_EMAIL,
+name AS USER_NAME  
+FROM users;
+-- SEE YOUR VIEWS
+SELECT * FROM my_v1;
++----+-------------------+--------+
+| id | email             | name   |
++----+-------------------+--------+
+|  1 | bob@dylan.com     | Bob    |
+|  2 | sylvie@dylan.com  | Sylvie |
+|  4 | aisha@khalfan.com | Aisha  |
++----+-------------------+--------+
+SELECT * FROM my_v2;
++---------+-------------------+-----------+
+| USER_ID | USER_EMAIL        | USER_NAME |
++---------+-------------------+-----------+
+|       1 | bob@dylan.com     | Bob       |
+|       2 | sylvie@dylan.com  | Sylvie    |
+|       4 | aisha@khalfan.com | Aisha     |
++---------+-------------------+-----------+
